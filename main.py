6260d6512e590a65c3fc8b3780c7b0e51609cb10
Panas18 <panas.tiwary@gmail.com>
2021-10-05 13:29:17 +0545

    Calculated fps

--- main.py
+++ main.py
@@ -4,25 +4,23 @@ import time
 cap = cv.VideoCapture("cars.mp4")
 prev_frame_time = 0
 new_frame_time = 0
-count = 0
 
-while cap.isOpened():
-    _, img = cap.read()
-    new_frame_time = time.time()
-    fps = 1/(new_frame_time - prev_frame_time)
-    prev_frame_time = new_frame_time
-    fps = str(int(fps))
-    if count % 5 == 0:
-        cv.putText(img, fps, (100, 100), cv.FONT_HERSHEY_SIMPLEX,
-                   3, (100, 255, 0), 3, cv.LINE_AA)
-    height, width = img.shape[0] // 2, img.shape[1]//2
-    dim = (width, height)
-    resized = cv.resize(img, dim, interpolation=cv.INTER_AREA)
-    count += 1
-    #cv.imshow("frame", img)
-    cv.imshow("frame", resized)
-    if cv.waitKey(1) & 0xff == ord("q"):
-        break
+try:
+    while cap.isOpened():
+        _, img = cap.read()
+        height, width = img.shape[0] // 2, img.shape[1]//2
+        dim = (width, height)
+        resized = cv.resize(img, dim, interpolation=cv.INTER_AREA)
+        cv.imshow("frame", resized)
+        new_frame_time = time.time()
+        time_taken = new_frame_time - prev_frame_time
+        prev_frame_time = new_frame_time
+        print(f"Fps {1/time_taken}")
+        if cv.waitKey(12) & 0xff == ord("q"):
+            break
 
-cap.release()
-cv.destroyAllWindows()
+    cap.release()
+    cv.destroyAllWindows()
+
+except AttributeError:
+    pass
