b55521004da8febd52dbb2f84758ab2a38ac991a
Panas18 <panas.tiwary@gmail.com>
2021-10-06 10:34:51 +0545

    background seperation

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import cv2 as cv
 import time
 
-new_frame_time, prev_frame_time = (0, 0)
-dim = (960, 540)
-cap = cv.VideoCapture("cars.mp4")
+KERNEL = cv.getStructuringElement(cv.MORPH_ELLIPSE, (2, 2))
+FGBG = cv.createBackgroundSubtractorKNN()
+DIM = (960, 540)
+
+
+def background_sub(img, kernel, fgbg):
+    fgmask = fgbg.apply(img)
+    fgmask = cv.morphologyEx(fgmask, cv.MORPH_OPEN, kernel)
+    return fgmask
 
 
 def calculate_fps(new_frame_time, prev_frame_time):
@@ -17,7 +23,7 @@ def calculate_fps(new_frame_time, prev_frame_time):
 
 def create_lines(img, a1=357, a2=232, b1=159, b2=279,
                  c1=467, c2=260, d1=324, d2=341):
-    color = (0, 255, 0)
+    color = (255, 255, 255)
     cv.line(img, (a1, a2), (c1, c2), color, 1)
     cv.line(img, (a1, a2), (b1, b2), color, 1)
     cv.line(img, (c1, c2), (d1, d2), color, 1)
@@ -26,18 +32,24 @@ def create_lines(img, a1=357, a2=232, b1=159, b2=279,
 
 if __name__ == "__main__":
     try:
+        new_frame_time, prev_frame_time = (0, 0)
+        cap = cv.VideoCapture("cars.mp4")
+
         while cap.isOpened():
             new_frame_time, prev_frame_time = calculate_fps(
                 new_frame_time, prev_frame_time)
             _, img = cap.read()
-            resized = cv.resize(img, dim, interpolation=cv.INTER_AREA)
-            create_lines(resized)
-            cv.imshow("frame", resized)
-            if cv.waitKey(12) & 0xff == ord("q"):
+            resized = cv.resize(img, DIM, interpolation=cv.INTER_AREA)
+            fgmask = background_sub(resized, KERNEL, FGBG)
+            create_lines(fgmask)
+            cv.imshow("bgmask", fgmask)
+            cv.imshow("img", resized)
+            if cv.waitKey(1) & 0xff == ord("q"):
                 break
 
-    except Exception as e:
-        print(e)
+    except Exception:
+        # print(e)
+        pass
 
     cap.release()
     cv.destroyAllWindows()
