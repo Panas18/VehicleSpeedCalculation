7f9f602918e452bf16e20d96b58ed0dd9a263215
Panas18 <panas.tiwary@gmail.com>
2021-10-07 09:21:31 +0545

    vehicle detected

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import cv2 as cv
 import time
 
+VIDEO = 'highway.mp4'
 KERNEL = cv.getStructuringElement(cv.MORPH_ELLIPSE, (2, 2))
 FGBG = cv.createBackgroundSubtractorKNN()
-DIM = (960, 540)
+AREA = 80
 
 
 def background_sub(img, kernel, fgbg):
@@ -21,35 +22,33 @@ def calculate_fps(new_frame_time, prev_frame_time):
     return new_frame_time, prev_frame_time
 
 
-def create_lines(img, a1=357, a2=232, b1=159, b2=279,
-                 c1=467, c2=260, d1=324, d2=341):
-    color = (255, 255, 255)
-    cv.line(img, (a1, a2), (c1, c2), color, 1)
-    cv.line(img, (a1, a2), (b1, b2), color, 1)
-    cv.line(img, (c1, c2), (d1, d2), color, 1)
-    cv.line(img, (b1, b2), (d1, d2), color, 1)
+def bounding_box(fgmask, img):
+    contours, _ = cv.findContours(fgmask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    for cnt in contours:
+        area = cv.contourArea(cnt)
+        if area > AREA:
+            cv.drawContours(img, [cnt], -1, (0, 255, 0), 2)
 
 
 if __name__ == "__main__":
     try:
         new_frame_time, prev_frame_time = (0, 0)
-        cap = cv.VideoCapture("cars.mp4")
+        cap = cv.VideoCapture(VIDEO)
 
-        while cap.isOpened():
+        while True:
             new_frame_time, prev_frame_time = calculate_fps(
                 new_frame_time, prev_frame_time)
             _, img = cap.read()
-            resized = cv.resize(img, DIM, interpolation=cv.INTER_AREA)
-            fgmask = background_sub(resized, KERNEL, FGBG)
-            create_lines(fgmask)
+            fgmask = background_sub(img, KERNEL, FGBG)
+            bounding_box(fgmask, img)
             cv.imshow("bgmask", fgmask)
-            cv.imshow("img", resized)
-            if cv.waitKey(1) & 0xff == ord("q"):
+            cv.imshow("img", img)
+            key = cv.waitKey(27)
+            if key == 27:
                 break
 
-    except Exception:
-        # print(e)
-        pass
+    except Exception as e:
+        print(e)
 
     cap.release()
     cv.destroyAllWindows()
